448cc148d6bd412e9ecac40099f2a0b82d67675c
Sanskriti Lakhmani <60683281+sanskritilakhmani@users.noreply.github.com>
2021-05-29 19:26:24 +0530

    Create Average_Population.sql

---
+++ SQL/Agrregation/Average_Population.sql
@@ -0,0 +1,8 @@
+/*
+Query the average population for all cities in CITY, rounded down to the nearest integer.
+
+Input Format
+
+The CITY table is described as follows: CITY.jpg
+*/
+Select round(avg(POPULATION)) From CITY ;
