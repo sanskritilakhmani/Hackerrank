1683741c71154c450139143f4363c1cf41b7e999
Sanskriti Lakhmani <60683281+sanskritilakhmani@users.noreply.github.com>
2021-05-29 18:42:25 +0530

    Create Triangles.sql

---
+++ SQL/Advanced_Select/Triangles.sql
@@ -0,0 +1,37 @@
+/*Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
+
+Equilateral: It's a triangle with  sides of equal length.
+Isosceles: It's a triangle with  sides of equal length.
+Scalene: It's a triangle with  sides of differing lengths.
+Not A Triangle: The given values of A, B, and C don't form a triangle.
+Input Format
+
+The TRIANGLES table is described as follows:
+
+
+
+Each row in the table denotes the lengths of each of a triangle's three sides.
+
+Sample Input
+
+
+
+Sample Output
+
+Isosceles
+Equilateral
+Scalene
+Not A Triangle
+*/
+/*
+Enter your query here.
+*/
+    
+    SELECT
+  CASE 
+    WHEN A + B <= C or A + C <= B or B + C <= A THEN 'Not A Triangle'
+    WHEN A = B and B = C THEN 'Equilateral'
+    WHEN A = B or A = C or B = C THEN 'Isosceles'
+    WHEN A <> B and B <> C THEN 'Scalene'
+  END tuple
+FROM TRIANGLES;
