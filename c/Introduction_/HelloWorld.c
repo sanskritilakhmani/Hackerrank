cbce8fc840f6667b1d809683b2787a62ac88e517
Sanskriti Lakhmani <60683281+sanskritilakhmani@users.noreply.github.com>
2021-05-29 14:16:58 +0530

    Update and rename HelloWorld to HelloWorld.c

--- c/Introduction_/HelloWorld.c
+++ c/Introduction_/HelloWorld.c
@@ -1,10 +1,12 @@
-"""Objective
-
-In this challenge, we will learn some basic concepts of C that will get you started with the language. You will need to use the same syntax to read input and write output in many C challenges. As you work through these problems, review the code stubs to learn about reading from stdin and writing to stdout.
+/*Objective
+In this challenge, we will learn some basic concepts of C that will get you started with the language. 
+You will need to use the same syntax to read input and write output in many C challenges. As you work through
+these problems, review the code stubs to learn about reading from stdin and writing to stdout.
 
 Task
 
-This challenge requires you to print  on a single line, and then print the already provided input string to stdout. If you are not familiar with C, you may want to read about the printf() command.
+This challenge requires you to print  on a single line, and then print the already provided
+input string to stdout. If you are not familiar with C, you may want to read about the printf() command.
 
 Example
 
@@ -32,8 +34,8 @@ Welcome to C programming.
 Sample Output 0
 
 Hello, World!
-Welcome to C programming.
-"""
+Welcome to C programming.*/
+	
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
