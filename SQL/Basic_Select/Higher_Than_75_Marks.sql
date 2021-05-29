b3ae7e552199542b8abd37210a9d1e2082c2942b
Sanskriti Lakhmani <60683281+sanskritilakhmani@users.noreply.github.com>
2021-05-29 14:23:56 +0530

    Update and rename Higher_Than_75_Marks to Higher_Than_75_Marks.sql

--- SQL/Basic_Select/Higher_Than_75_Marks.sql
+++ SQL/Basic_Select/Higher_Than_75_Marks.sql
@@ -1,4 +1,4 @@
-"""
+/*
 Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
 
 Input Format
@@ -17,8 +17,9 @@ Belvet
 Explanation
 
 Only Ashley, Julia, and Belvet have Marks > . If you look at the last three characters of each of their names, there are no duplicates and 'ley' < 'lia' < 'vet'.
-"""
+
 For MS SQL Server, you can use RIGHT function like;
+*/
 
 SELECT NAME FROM STUDENTS WHERE MARKS > 75 ORDER BY RIGHT(NAME, 3), ID ASC;
 
