80cc577bd7682be6d22d8b99ef682bd689b97f03
Sanskriti Lakhmani <60683281+sanskritilakhmani@users.noreply.github.com>
2021-05-31 16:40:11 +0530

    Create More_Dice.py

---
+++ 10_Days_of_Statistics/Day2/More_Dice.py
@@ -0,0 +1,8 @@
+
+# Day 2: More Dice
+In a single toss of 2 fair (evenly-weighted) six-sided dice, find the probability that the values rolled by each die will be different and the two dice have a sum of 6.
+= P(1,5) + P(2,4) + P(4,2) + P(5,1)
+= (1/6*1/6) + (1/6*1/6) + (1/6*1/6) + (1/6*1/6)
+= 4/36
+= 1/9
+Answer: 1/9
