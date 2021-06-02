61d16ae315d0ccb2e1344d43708101aa94ab8d09
Sanskriti Lakhmani <60683281+sanskritilakhmani@users.noreply.github.com>
2021-06-02 09:56:10 +0530

    Create Cards_of_same_suit.c

---
+++ 10_Days_of_Statistics/Day3/Cards_of_same_suit.c
@@ -0,0 +1,7 @@
+
+# Day 3: Cards of the Same Suit
+You draw 2 cards from a standard 52-card deck without replacing them. What is the probability that both cards are of the same suit?
+= 52/52 * 12/51
+= 1 * 12/51
+= 12/51
+Answer: 12/51
