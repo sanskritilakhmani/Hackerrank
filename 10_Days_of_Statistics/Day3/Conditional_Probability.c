918cb6f15f5d6093a54a077919caa2aa8cd4d7b2
Sanskriti Lakhmani <60683281+sanskritilakhmani@users.noreply.github.com>
2021-06-02 09:55:23 +0530

    Create Conditional_Probability.c

---
+++ 10_Days_of_Statistics/Day3/Conditional_Probability.c
@@ -0,0 +1,8 @@
+
+# Day 3: Conditional Probability
+Suppose a family has 2 children, one of which is a boy. What is the probability that both children are boys?
+All possible outcomes: BB, BG, GB, GG
+Given one of them is a boy, all possible outcomes become: BB, BG, GB
+ie, denominator becomes 3
+P(BB) = 1/3
+Answer: 1/3
