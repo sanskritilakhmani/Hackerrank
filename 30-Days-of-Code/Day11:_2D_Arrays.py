104a36a38d4768f2a26150995f8cbcf0c9ef8f72
Sanskriti Lakhmani <60683281+sanskritilakhmani@users.noreply.github.com>
2021-06-02 10:11:25 +0530

    Create Day11:_2D_Arrays.py

---
+++ 30-Days-of-Code/Day11:_2D_Arrays.py
@@ -0,0 +1,90 @@
+"""
+Objective
+Today, we are building on our knowledge of arrays by adding another dimension. Check out the Tutorial tab for learning materials and an instructional video.
+
+Context
+Given a  2D Array, :
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+We define an hourglass in  to be a subset of values with indices falling in this pattern in 's graphical representation:
+
+a b c
+  d
+e f g
+There are  hourglasses in , and an hourglass sum is the sum of an hourglass' values.
+
+Task
+Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum.
+
+Example
+
+In the array shown above, the maximum hourglass sum is  for the hourglass in the top left corner.
+
+Input Format
+
+There are  lines of input, where each line contains  space-separated integers that describe the 2D Array .
+
+Constraints
+
+Output Format
+
+Print the maximum hourglass sum in .
+
+Sample Input
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+Sample Output
+
+19
+Explanation
+
+ contains the following hourglasses:
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  1       0       0       0
+1 1 1   1 1 0   1 0 0   0 0 0
+
+0 1 0   1 0 0   0 0 0   0 0 0
+  1       1       0       0
+0 0 2   0 2 4   2 4 4   4 4 0
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  0       2       4       4
+0 0 0   0 0 2   0 2 0   2 0 0
+
+0 0 2   0 2 4   2 4 4   4 4 0
+  0       0       2       0
+0 0 1   0 1 2   1 2 4   2 4 0
+The hourglass with the maximum sum () is:
+
+2 4 4
+  2
+"""
+
+import math
+import os
+import random
+import re
+import sys
+
+arr = []
+for _ in range(6):
+    arr.append(list(map(int, input().rstrip().split())))
+
+hourglass_sum = []
+for i in range(0,4):
+    for j in range(0,4):
+        total = sum([sum(x[i:i+3]) for x in arr[j:j+3]]) - arr[j+1][i] - arr[j+1][i+2]
+        hourglass_sum.append(total)
+
+print(max(hourglass_sum))
