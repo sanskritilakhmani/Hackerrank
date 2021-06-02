60c9c3d7a04cc723de3b3774b35b5cbdd8683f3a
Sanskriti Lakhmani <60683281+sanskritilakhmani@users.noreply.github.com>
2021-06-02 09:56:38 +0530

    Create Drawing_Marbles.c

---
+++ 10_Days_of_Statistics/Day3/Drawing_Marbles.c
@@ -0,0 +1,6 @@
+
+# Day 3: Drawing Marbles
+A bag contains 3 red marbles and 4 blue marbles. Then, 2 marbles are drawn from the bag, at random, without replacement. If the first marble drawn is red, what is the probability that the second marble is blue?
+If first marble drawn is red, bag now has 2 red marbles and 4 blue marbles
+P(second marble is blue) = 4 / (2+4) = 4/6 = 2/3
+Answer: 2/3
